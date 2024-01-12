ab6351861a320746090c53d33ec1df81278cc013
emmanuel-903 <155675631+emmanuel-903@users.noreply.github.com>
2024-01-12 19:14:31 +0000

    commit emma2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getRandomColor(){
+function CambiarColores(){
   const letters = '0123456789ABCDEF';
   let color = '#';
   for(let i = 0; i < 6; i++ ){
@@ -8,9 +8,9 @@ function getRandomColor(){
 }
 function applyRandomColors() {
   const h5Element = document.querySelector('h5');
-  h5Element.style.color = getRandomColor();
-  h5Element.style.backgroundColor = getRandomColor();
-  h5Element.style.borderBottom = `2px solid ${getRandomColor()}`;
+  h5Element.style.color = CambiarColores();
+  h5Element.style.backgroundColor = CambiarColores();
+  h5Element.style.borderBottom = `2px solid ${CambiarColores()}`;
 }
 
 // Llamamos a la función para que se aplique al cargar la página
