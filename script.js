8b9755545306f51fa7ca01211ce3b0d0976edb6e
Osvaldoisv <154470162+Osvaldoisv@users.noreply.github.com>
2024-01-12 20:07:25 +0000

    Colores aleatorios con click listos

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-function CambiarColores(){
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for(let i = 0; i < 6; i++ ){
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-function applyRandomColors() {
-  const h5Element = document.querySelector('h5');
-  h5Element.style.color = CambiarColores();
-  h5Element.style.backgroundColor = CambiarColores();
-  h5Element.style.borderBottom = `2px solid ${CambiarColores()}`;
-}
+function CambiarColores() {
+  const tituloH5 = document.querySelector(`h5`);
 
-// Llamamos a la función para que se aplique al cargar la página
-applyRandomColors();
+  let numeroAleatorio = Math.random();
+  let numeroEntre1y3 = Math.floor(numeroAleatorio * 3) + 1;
+  let colores = ["red", "blue", "green"];
+
+  tituloH5.style.color = colores[numeroEntre1y3 - 1];
+}
